310d8693f565bb4ec4ccd3927cf00ae50d80adf6
Jairo alexander Valencia candamil <jvalenci@z4r9p1.42lyon.fr>
2021-12-15 13:58:19 +0100

    update

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,11 +6,11 @@
 /*   By: jvalenci <jvalenci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 11:48:36 by jvalenci          #+#    #+#             */
-/*   Updated: 2021/12/13 11:48:38 by jvalenci         ###   ########.fr       */
+/*   Updated: 2021/12/15 13:48:23 by jvalenci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include"get_next_line.h"
+#include"get_next_line_bonus.h"
 
 void	ft_free(char **buffer)
 {
--- get_next_line_utils_bonus.c
+++ get_next_line_utils_bonus.c
@@ -6,11 +6,11 @@
 /*   By: jvalenci <jvalenci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 11:48:57 by jvalenci          #+#    #+#             */
-/*   Updated: 2021/12/13 11:48:59 by jvalenci         ###   ########.fr       */
+/*   Updated: 2021/12/15 13:48:06 by jvalenci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include"get_next_line.h"
+#include"get_next_line_bonus.h"
 
 size_t	ft_strlen(const char *s)
 {
